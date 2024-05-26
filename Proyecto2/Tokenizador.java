1c7cc3d62298dc30207f5eabb59457aa9e18cebd
An-G <150113496+An-Gi@users.noreply.github.com>
2024-05-26 11:43:41 -0600

    Update Tokenizador.java

--- Proyecto2/Tokenizador.java
+++ Proyecto2/Tokenizador.java
@@ -1,1 +1,31 @@
+package Proyecto2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Tokenizador {
+    private final LectorDeArchivo lector;
+    private final String contenido;
+    public List<String> listaTokens;
+    public Tokenizador(LectorDeArchivo lector) {
+        this.lector = lector;
+        this.contenido = lector.getContenidoArchivo();
+        this.listaTokens = new ArrayList<>();
+    }
+
+    public void guardarTokens() {
+        StringTokenizer st = new StringTokenizer(contenido, " []()*&^%$#+@-,:';¿¡!?.", true);
+        while (st.hasMoreTokens()) {
+            String token = st.nextToken();
+            token = token.replace("\n", "").replace("\r", "");
+            if ((!token.equals(" ")&&(!token.trim().isEmpty()))) {
+                listaTokens.add(token);
+            }
+        }
+    }
+    public List<String> getListaTokens() {
+        return listaTokens;
+    }
+}
 
